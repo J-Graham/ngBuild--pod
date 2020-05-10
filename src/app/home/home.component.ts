b2375e44562e9905d6533b8e11db9c104b08c45c
John Graham <johngrhm@gmail.com>
2020-05-10 17:29:25 -0400

    More updates
    Added Bio and images
    Added Logo
    Added most recent episode

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,21 +3,21 @@ import { Observable } from 'rxjs';
 import { ScullyRoutesService } from '@scullyio/ng-lib';
 import { RssService } from './rss.service';
 
-
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+    recentEpisodes$: Observable<any>;
 
-  recentEpisodes$: Observable<any>;
+    constructor(private rss: RssService) {}
 
-  constructor (private rss: RssService) { }
+    ngOnInit() {
+        this.recentEpisodes$ = this.rss.getFeed();
+    }
 
-  ngOnInit() {
-
-    this.recentEpisodes$ = this.rss.getFeed();
-  }
-
+    getDuration(seconds: number): string {
+        return new Date(seconds * 1000).toISOString().substr(11, 8);
+    }
 }
